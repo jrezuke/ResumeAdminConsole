2aeee6b59c04c521cb67c3d27b1e8284ff7fb5b1
jrezuke <j.rezuke@verizon.net>
2015-03-08 08:36:36 -0400

    initial commits

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ResumeAdmin.Data;
+
+namespace ResumeAdminConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = new ResumeDbContext();
+            var resumes = db.Resumes.ToList();
+        }
+    }
+}
